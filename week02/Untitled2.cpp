cd85fb2f273374e5ddd42dae11c69e9991f9e1de
rongrongrong1111 <14751052@me.mcu.edu.tw>
2026-09-17 11:42:08 +0800

    week02

---
+++ week02/Untitled2.cpp
@@ -0,0 +1,12 @@
+///week02-2.cpp 要使用命名空間
+#include <iostream>
+#include <string>
+using namespace std;///使用std命名空間
+int main()
+{
+    cout << "請問你叫什麼名字啊?";
+    string name;///宣告字串 name
+    cin >> name;///上週教cin 原來長這樣
+    cout << name << "你好，今天教命名空間喔";
+}
+
