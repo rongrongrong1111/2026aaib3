eb5f346e41154b14fa044d47947d84910ec01db8
rongrongrong1111 <14751052@me.mcu.edu.tw>
2026-09-24 11:12:47 +0800

    week03

---
+++ week03/week03-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int smallestIndex(vector<int>& nums) {
+        //把sum[i]每個位數加起來，是否==i
+        for(int i=0;i<nums.size();i++){//陣列逐一檢查
+            int total =0;//把sum[i]每個位數加起來
+            while(nums[i]>0){//剝皮法
+                total+=nums[i]%10;
+                nums[i]=nums[i]/10;
+            }
+            if(total == i)return i;
+        }
+        return -1;//不是，就-1
+    }
+};
